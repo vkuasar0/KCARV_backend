073717947f54487948eb1bcc11a8e13b7af1b0ff
Sanjan-rao <usanjanrao@gmail.com>
2024-08-31 14:21:29 +0530

    Allowing only admin to delete announcements

--- routes/announcementRoutes.js
+++ routes/announcementRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { createAnnouncement, getAnnouncements, deleteAnnouncements } = require('../controllers/announcementController');
 const router = express.Router();
+const { authenticateUser, isAdmin } = require('../middlewares/authMiddleware');
 
 router.post('/announcements', createAnnouncement);
 router.get('/announcements', getAnnouncements);
-router.delete('/announcements/:id', deleteAnnouncements)
+router.delete('/announcements/:id', authenticateUser, isAdmin, deleteAnnouncements)
 
 module.exports = router;
